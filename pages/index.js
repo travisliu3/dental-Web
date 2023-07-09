d128602ad98afc359ad81b91e888cd9cb216b7a7
Travis Liu <travisliu3@gmail.com>
2023-07-09 18:35:42 -0400

    prototype

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <img
+      {/* <img
         src={`https://www.ironwooddental.com/hubfs/Blog_images/cosmetic_dentistry-n4o2dplupts9ys28zlmqjmn6y3c3h7zy133nsv04oo.jpg`}
         style={{
           zIndex: -1,
@@ -19,45 +19,43 @@ export default function Home() {
           opacity: 0.8,
           position: 'absolute'
         }}
-      />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Container className='p-5'>
-        <Row>
-          <h1>Welcome to Cosmetic Dental Clinic</h1>
-        </Row>
+      /> */}
+      <div id='topBack'>
         <br />
-        <Row>
-          <Col lg={6}>
-            <p className='fs-5'>
-              Experience exceptional full-service dental care in Bokaro Steel City. Our professionl environment
-              and skilled team ensure your utmost satisfaction. Visit us for personalized, comprehensive treatment
-              and leave with a confident smile.
-            </p>
-          </Col>
-        </Row>
-        <Button className='bg-info m-2'>
-          <Link className='text-white fw-bold text-decoration-none' href={'/appointment'}>
-            Book Appointment
-          </Link>
-        </Button>
-        <Button className='bg-info m-2'>
-          <Link className='text-white fw-bold text-decoration-none' href={'tel:9431128804'}>
-            Call 943-112-8804
-          </Link>
-        </Button>
-      </Container>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Container className='p-5'>
+          <Row>
+            <h1>Welcome to Cosmetic Dental Clinic</h1>
+          </Row>
+          <br />
+          <Row>
+            <Col lg={6}>
+              <p className='fs-5'>
+                Experience exceptional full-service dental care in Bokaro Steel City. Our professionl environment
+                and skilled team ensure your utmost satisfaction. Visit us for personalized, comprehensive treatment
+                and leave with a confident smile.
+              </p>
+            </Col>
+          </Row>
+          <Button className='bg-info m-2'>
+            <Link className='text-white fw-bold text-decoration-none' href={'/appointment'}>
+              Book Appointment
+            </Link>
+          </Button>
+          <Button className='bg-info m-2'>
+            <Link className='text-white fw-bold text-decoration-none' href={'tel:9431128804'}>
+              Call 943-112-8804
+            </Link>
+          </Button>
+        </Container>
+        <br />
+        <br />
+        <br />
+      </div>
       <Container className='p-5'>
         <Row>
           <Col lg={6}>
